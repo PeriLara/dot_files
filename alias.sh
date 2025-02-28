7fb4bb36731cdafd6237de4c53f0ee238c0a7449
Lara Perinetti <l.perinetti@qwant.com>
2025-02-28 15:51:38 +0100

    update

--- alias.sh
+++ alias.sh
@@ -1,22 +1,19 @@
-alias la="ls -a"
+
+alias ..='cd ..'
+alias la="lsd -a"
+alias ls="lsd"
+alias lla="ls -la"
+alias lt="ls --tree"
 
 mkcd ()
 {
   mkdir -p -- "$1" && cd -P -- "$1"
 }
 
-alias cd_vespa="cd ~/Workspace/Vespa/vespa-projects"
-
-alias sshvpn="ssh hdfs@10.103.1.1"
-alias sshdgx2="ssh l.perinetti@10.100.2.2"
-
-alias vespa="pyenv activate vespa"
-alias mkdoc="pyenv activate mkdocs"
-alias sec="pyenv activate sec"
-alias qml="pyenv activate qml"
+alias ibrew='arch --x86_64 /usr/local/Homebrew/bin/brew'
+alias iarch='arch -x86_64 zsh'
 
 alias jupyt="nohup jupyter lab &"
 
-alias black_qml="cd ~/Workspace/qwant-ml/ | black qwant_ml tests scripts" 
 
 alias pdfs='docker run -ti -e HADOOP_USER_NAME=hdfs -e HADOOP_CONF_DIR=/opt/hadoop/etc/hadoop -v $(pwd):/data --rm apache/hadoop:3 hdfs dfs -fs hdfs://10.103.1.1:8020'
